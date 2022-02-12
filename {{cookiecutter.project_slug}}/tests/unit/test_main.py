2e60900815c9cfb373c6199667bd07d1c64750bc
Paula Ceccon <paulaceccon4@gmail.com>
2022-02-12 16:23:19 -0300

    python cli

---
+++ {{cookiecutter.project_slug}}/tests/unit/test_main.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from unittest.mock import MagicMock, patch
+
+from {{cookiecutter.project_slug}}.main import main
+
+
+@patch("builtins.print")
+def test_run(print_mock: MagicMock):
+    # Given
+    from_datetime = datetime.strptime("2021-01-01", "%Y-%m-%d").date()
+    # When
+    main(from_datetime)
+
+    # Then
+    print_mock.assert_called_once_with("Run on date from '2021-01-01'")
